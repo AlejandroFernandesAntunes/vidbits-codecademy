32a5e4ea5ef3da85caf3afdc10b9736efc440adc
Alejandro Fernandes Antunes <aantunes70@hotmail.com>
2018-03-07 11:03:44 -0300

    Steps 16 and 17

--- routes/videos.js
+++ routes/videos.js
@@ -2,7 +2,14 @@ const router = require('express').Router();
 const Video = require('../models/video');
 module.exports = router;
 
-// POST video route
+router.get('/', (req, res) =>{
+  res.render('index')
+})
+
+router.get('/create', async (req, res, next) => {
+  res.render('create');
+});
+
 router.post('/videos', async (req, res) => {
   const {title, description} = req.body;
   const newVideo = new Video({title, description});
